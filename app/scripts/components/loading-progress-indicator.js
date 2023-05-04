845386268994bb0b59728c904e51898c96498803
Alesei N <github.com@bzik.net>
2023-05-04 09:48:31 -0500

    feat: :sparkles: adding progress loader indicator component for later use

---
+++ app/scripts/components/loading-progress-indicator.js
@@ -0,0 +1,7 @@
+/**
+ * This is component show spinner that indicates that something is still in progress.
+ * 
+ */
+import React from 'react';
+
+<div class="loading-progress-indicator"><div></div><div></div><div></div><div></div></div>
