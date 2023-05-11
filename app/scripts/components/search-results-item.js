3c58c7a181b490dc983fc42d5b921ad5d7b15e4d
Alesei N <github.com@bzik.net>
2023-05-11 15:53:13 -0500

    fix: :recycle: missed renaming div to li when intially split the component into two

--- app/scripts/components/search-results-item.js
+++ app/scripts/components/search-results-item.js
@@ -16,14 +16,14 @@ const SearchResultsItem = (props) => {
   } = itemData;
 
   return (
-    <div>
+    <li>
       {isActive ? (<b>Active Item</b>) : null}
       {picture ? (<img src={picture} />) : null}
       {name ? (<h1>{name}</h1>) : null}
       {price ? (<h2>${price}</h2>) : null}
       {about ? (<p>{about}</p>) : null}
       {tags ? (<ul>{tags.map((tagValue, tagIndex) => <li index={tagIndex}>{tagValue}</li>)}</ul>) : null}
-    </div>
+    </li>
   );
 }
 
